68d80f7efd7803828995fe33feaefbe36bb6b138
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2020-12-31 11:41:16 +0100

    fixed indenting bug

--- bot.py
+++ bot.py
@@ -24,10 +24,10 @@ for x in range(5000):
         print("Playing song...")
     except:
         try:
-        driver.find_element_by_xpath(
-            '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/div[3]/div/button[2]/span[2]/span/span[1]')
+            driver.find_element_by_xpath(
+                '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/div[3]/div/button[2]/span[2]/span/span[1]')
 
-        print("Playing song...")
+            print("Playing song...")
         except expression as identifier:
             print("Couldn't play song")
 
