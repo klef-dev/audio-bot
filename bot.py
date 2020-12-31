dd7da4aaf4fb21f79ab20f4b41429bcb5047ecbd
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2020-12-31 17:02:02 +0100

    Fixed bug

--- bot.py
+++ bot.py
@@ -4,8 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-chrome_options = Options()
-
 print("Opening Browser...")
 
 driver = webdriver.Chrome()
