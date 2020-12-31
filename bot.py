4a1e0828d37b9db0839d0d83e4099a06fb9ab4e6
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2020-12-31 16:48:41 +0100

    Improved on playing song correctly

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = Options()
 
 print("Opening Browser...")
 
-chrome_options.add_argument("--headless")
-
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome()
 
 for x in range(5000):
     print("Requesting BR's page...")
+    driver.implicitly_wait(10)
     driver.get("https://audiomack.com/blessedray/album/prodigy-ep-vol-1")
     sleep(10)
 
@@ -19,47 +20,30 @@ for x in range(5000):
 
     try:
         driver.find_element_by_xpath(
-            '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/ul/li/button[1]').click()
+            '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/div[1]/div/button[2]').click()
 
-        print("Playing song...")
+        WebDriverWait(driver, 60).until(
+            EC.visibility_of_element_located(
+                (By.CLASS_NAME, "play-button--playing"))
+        )
+
+        print("Playing 1 song...")
+
+        sleep(5)
+
+        driver.find_element_by_xpath(
+            '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/div[2]/div/button[2]').click()
+
+        WebDriverWait(driver, 60).until(
+            EC.visibility_of_element_located(
+                (By.CLASS_NAME, "play-button--playing"))
+        )
+
+        print("Playing 2 song...")
+
+        sleep(5)
     except:
-        try:
-            driver.find_element_by_xpath(
-                '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/div[1]/div/button[2]/span[2]/span/span[1]').click()
-
-            print("Playing 1 song...")
-
-            sleep(10)
-
-            driver.find_element_by_xpath(
-                '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/div[2]/div/button[2]/span[2]/span/span[1]').click()
-
-            print("Playing 2 song...")
-
-            sleep(10)
-
-            driver.find_element_by_xpath(
-                '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/div[3]/div/button[2]/span[2]/span/span[1]').click()
-
-            print("Playing 3 song...")
-
-            sleep(10)
-
-            driver.find_element_by_xpath(
-                '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/div[4]/div/button[2]/span[2]/span/span[1]').click()
-
-            print("Playing 4 song...")
-
-            sleep(10)
-
-            driver.find_element_by_xpath(
-                '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/div[5]/div/button[2]/span[2]/span/span[1]').click()
-
-            print("Playing 5 song...")
-
-            sleep(10)
-        except:
-            print("Couldn't play song")
+        print("Couldn't play song")
 
 driver.quit()
 print("Done ✅")
