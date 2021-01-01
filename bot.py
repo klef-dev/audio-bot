ff341af60e2080219f0d2e973790f19e2912ef51
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2021-01-01 21:20:39 +0100

    Added schedule to run daily

--- bot.py
+++ bot.py
@@ -1,56 +1,70 @@
+#!/usr/bin/env python3
+
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+import schedule
 
-for x in range(5000):
-    chrome_options = Options()
 
-    print("Opening Browser...")
+def botiomack():
+    for x in range(5000):
+        chrome_options = Options()
 
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
+        print("Opening Browser...")
 
-    driver = webdriver.Chrome(options=chrome_options)
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
 
-    print("Requesting BR's page...")
+        driver = webdriver.Chrome(options=chrome_options)
 
-    driver.get("https://audiomack.com/blessedray/album/prodigy-ep-vol-1")
+        print("Requesting BR's page...")
 
-    print("Trying to play song..")
+        driver.get("https://audiomack.com/blessedray/album/prodigy-ep-vol-1")
 
-    try:
-        btn1 = driver.find_element_by_xpath(
-            '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/ul/li/button[1]')
-
-        driver.execute_script("arguments[0].click();", btn1)
+        print("Trying to play song..")
 
         try:
-            paused = WebDriverWait(driver, 10).until(
+            btn1 = driver.find_element_by_xpath(
+                '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/ul/li/button[1]')
+
+            driver.execute_script("arguments[0].click();", btn1)
+
+            try:
+                paused = WebDriverWait(driver, 10).until(
+                    EC.visibility_of_element_located(
+                        (By.CLASS_NAME, "play-button--paused"))
+                )
+
+                if paused:
+                    driver.find_element_by_xpath(
+                        '//*[@id="react-view"]/div[7]/div/div[1]/button[2]').click()
+            except:
+                pass
+
+            playing = WebDriverWait(driver, 10).until(
                 EC.visibility_of_element_located(
-                    (By.CLASS_NAME, "play-button--paused"))
+                    (By.CLASS_NAME, "play-button--playing"))
             )
 
-            if paused:
-                driver.find_element_by_xpath(
-                    '//*[@id="react-view"]/div[7]/div/div[1]/button[2]').click()
+            print("Playing 1 song...")
+
+            sleep(2)
+
+            print("Done with ", format(x+1))
         except:
-            pass
+            print("Couldn't play song")
+        driver.quit()
+    print("Done ✅")
 
-        playing = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located(
-                (By.CLASS_NAME, "play-button--playing"))
-        )
 
-        print("Playing 1 song...")
+botiomack()
 
-        sleep(2)
+schedule.every().day.do(botiomack)
 
-        print("Done with ", format(x+1))
-    except:
-        print("Couldn't play song")
-    driver.quit()
-print("Done ✅")
+while True:
+    schedule.run_pending()
+    sleep(1)
