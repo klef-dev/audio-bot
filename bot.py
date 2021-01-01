a3abd1476ebf853a9e7dd216afde5e5d0fa34e28
root <root@cbs@klefcodes.net>
2021-01-01 18:39:30 +0000

    Commit from digitalocean

--- bot.py
+++ bot.py
@@ -4,7 +4,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from pyvirtualdisplay import Display
 
+display = Display(visible=0, size=(800, 800))  
+display.start()
 
 for x in range(5000):
     chrome_options = Options()
@@ -12,6 +15,10 @@ for x in range(5000):
     print("Opening Browser...")
 
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.binary_location = '/opt/google/chrome/google-chrome'
 
     driver = webdriver.Chrome(options=chrome_options)
 
@@ -23,7 +30,7 @@ for x in range(5000):
 
     try:
         btn1 = driver.find_element_by_xpath(
-            '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/div[1]/div/button[2]')
+            '//*[@id="react-view"]/div[3]/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/ul/li/button[1]')
 
         driver.execute_script("arguments[0].click();", btn1)
 
