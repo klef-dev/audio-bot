3f705dec52dede37155f08ceb388b4590976b72d
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2021-01-04 22:17:52 +0100

    ⚡️ Created favorite bot

---
+++ users.py
@@ -0,0 +1,72 @@
+users = [
+    {
+        "email": "UgochukwutubelumOkwukwe@armyspy.com",
+        "username": "Ugochukwutubelum",
+        "password": "Ugochukwutubelum"
+    },
+    {
+        "email": "AdaugoEsomchi@teleworm.us",
+        "username": "Adaugo",
+        "password": "Adaugo"
+    },
+    {
+        "email": "ChukwuebukaOdili@rhyta.com",
+        "username": "Chukwuebuka",
+        "password": "Chukwuebuka"
+    },
+    {
+        "email": "ObianujuOtitodilinna@armyspy.com",
+        "username": "Obianuju",
+        "password": "Obianuju"
+    },
+    {
+        "email": "IjeomaOnuchukwu@jourrapide.com",
+        "username": "Ijeoma",
+        "password": "Ijeoma"
+    },
+    {
+        "email": "NnennaOkoli@teleworm.us",
+        "username": "Nnenna",
+        "password": "Nnenna"
+    },
+    {
+        "email": "IfeanyichukwuOlejuru@rhyta.com",
+        "username": "Ifeanyichukwu",
+        "password": "Ifeanyichukwu"
+    },
+    {
+        "email": "AchebeOtitodilichukwu@dayrep.com",
+        "username": "Achebe",
+        "password": "Achebe"
+    },
+    {
+        "email": "ChiazagomekpereIloerika@rhyta.com",
+        "username": "Chiazagome",
+        "password": "Chiazagome"
+    },
+    {
+        "email": "NkirukaChidozie@rhyta.com",
+        "username": "Nkiruka",
+        "password": "Nkiruka"
+    },
+    {
+        "email": "ChiebukaAmadi@armyspy.com",
+        "username": "Chiebuka",
+        "password": "Chiebuka"
+    },
+    {
+        "email": "OtitodilichukwuEjikemeifeuwa@rhyta.com",
+        "username": "Otitodilichukwu",
+        "password": "Otitodilichukwu"
+    },
+    {
+        "email": "NnamdiChinwenma@jourrapide.com",
+        "username": "Nnamdi",
+        "password": "Nnamdi"
+    },
+    {
+        "email": "ChinyereTobenna@dayrep.com",
+        "username": "Chinyere",
+        "password": "Chinyere"
+    }
+]
